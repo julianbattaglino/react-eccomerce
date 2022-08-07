dd7a227c6c2522c9bb2ef03774b8c2f830287b95
Julian Battaglino <julianbattaglino@gmail.com>
2022-08-06 22:04:38 -0300

    CartWidget

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,13 +1,24 @@
 import { Image } from '@chakra-ui/react'
 import "./CartWidget.css";
 
+import { Link } from 'react-router-dom'
+import { useContext } from 'react';
+import CartContext from '../context/CartContext';
+
 const CartWidget = () => {
+    const { getQuantity } = useContext(CartContext)
+
+    const quantity = getQuantity()
+
+
     return (
+        <Link to='/cart'>
         <div className="cart-widget">
             <span className="cart-count"><Image className='cart-icon'
-                src="/img/cart-50.png"/>0</span>
+                src="/img/cart-50.png"/>{quantity}</span>
              
         </div>
+        </Link>
     );
 }
 
