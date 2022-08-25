f93f162485c349528a8695eb8d2718b7b2b50dad
Julian Battaglino <julianbattaglino@gmail.com>
2022-08-25 17:59:44 -0300

    add checkout Route

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { CartContextProvider } from './context/CartContext';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import Cart from './components/Cart/Cart';
+import Checkout from './components/Checkout/Checkout';
 import Navbar from './components/Navbar/Navbar';
 import NotFound404 from './components/NotFound404/NotFound404';
 import ScrollToTop from './components/ScrollToTop/ScrollToTop';
@@ -25,7 +26,8 @@ function App() {
           <Route path="/" element={<ItemListContainer greeting='Marcelo Kopp - Xilografias' />} />
           <Route path='/category/:categoryId' element={<ItemListContainer greeting='Categoria: ' /> } />
           <Route path='/detail/:productId' element={<ItemDetailContainer />} />
-          <Route path='/Cart' element={<Cart />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/checkout' element={<Checkout />} />
           <Route path="*" element={<NotFound404/>}  />
         </Routes>
         <ScrollToTop />
