9252bc514f0c3e8aa4b940412a030772707cb024
Julian Battaglino <julianbattaglino@gmail.com>
2022-08-29 23:17:44 -0300

    tabulations (for clean code)

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,7 +1,6 @@
 import "./ItemListContainer.css";
 import ItemList from '../ItemList/ItemList'
 
-
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom';
