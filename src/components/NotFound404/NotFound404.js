8985f55906af6985de20a7fff14d4cc6e418cf67
Julian Battaglino <julianbattaglino@gmail.com>
2022-08-29 23:19:32 -0300

    tabulations (for cleand code)

--- src/components/NotFound404/NotFound404.js
+++ src/components/NotFound404/NotFound404.js
@@ -1,16 +1,14 @@
-import { Image } from '@chakra-ui/react'
-
 import "./NotFound404.css"
-
+import { Image } from '@chakra-ui/react'
 
 const NotFound404 = () => {
 
     return (
         <>
-        <div className="not-found-container">
-            <Image className="not-found-img" src="/img/extermination.png" />
-        </div>
-        <h2 className='not-found-title'>ERROR 404 - PAGE NOT FOUND</h2>
+            <div className="not-found-container">
+                <Image className="not-found-img" src="/img/extermination.png" />
+            </div>
+            <h2 className='not-found-title'>ERROR 404 - PAGE NOT FOUND</h2>
 
         </>
     )
