94702884eba58892518e4441fdfca2440dd2b87e
Julian Battaglino <julianbattaglino@gmail.com>
2022-08-29 23:16:22 -0300

    tabulations (for clean code)

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,12 +1,12 @@
 import Item from "../Item/Item"
 import "./ItemList.css"
 
-const ItemList = ({products}) => {
-    return(
+const ItemList = ({ products }) => {
+    return (
         <div className="grid-products">
             {/*map method for products*/}
-            {products.map(prod => <Item key={prod.id} {...prod}/>)}
-        </div>      
+            {products.map(prod => <Item key={prod.id} {...prod} />)}
+        </div>
     )
 }
 
